d5b28b22f1a68e2333c746dae6c5192bbacb7d54
ladiwalparth <ladiwalparth@gmail.com>
2025-01-16 07:54:42 +0000

    Arithmetic operations

---
+++ 01_basics/lecture7.js
@@ -0,0 +1,12 @@
+// operations
+"use strict"
+console.log(2**3); // power
+console.log(2%3); // remainder
+console.log("1" + 2 + 2);
+console.log(1+2+"2");
+console.log(1+"2");
+console.log("1"+2);
+
+// above things good for only exams point of view but not prefered in practical life.
+
+// read about prefix and postfix increment operators at js mdn
