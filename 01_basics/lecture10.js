9ccfb54f9f32cc7d7873f27fc84d5a9c771e37e4
ladiwalparth <ladiwalparth@gmail.com>
2025-01-16 08:42:30 +0000

    lecture10

---
+++ 01_basics/lecture10.js
@@ -0,0 +1,26 @@
+"use strict"
+
+// Primitive datatypes occupy stack memory where always a copy is assigned
+// Non Primitive datatypes occupy heap memory where reference is assigned and any changes in one will affect all the variables.
+
+let a = 12
+let b = a
+
+++b;
+console.log(a);
+console.log(b);
+
+let obj = {
+    email: "ladiwalparth@gmail.com",
+    pass: "hare krishna"
+}
+
+let obj2 = obj;
+
+obj2.email = "parth@harekrishna"
+
+console.table([obj,obj2])
+
+// both gets changed.
+
+// arrays, objects, functions are example of non primitive datatypes.
